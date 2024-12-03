8bbfacac35d5e0029bb0ea8cb218133f9b34b3b6
kartavkun <nikitarobezhko@yandex.ru>
2024-12-03 18:24:47 +0500

    перелопачен на хой

--- main.py
+++ main.py
@@ -12,25 +12,13 @@ if not API_TOKEN:
     exit()
 
 initial_channels = [
-    "kartav__", "ivango", "senyawei", "v1llix_", "glebauster",
+    "kartav__", "ivango", "senyawei", "glebauster",
     "godroponika", "quizzzzz_", "vudek_", "pokemonyaaa", "25mosey",
-    "majewskiosu", "f0rz__", "steisha_owo", "sor0k4", "skyfai_",
+    "majewskiosu", "f0rz__", "steisha_owo", "danon_osu", "skyfai_",
     "zoomqge", "lofkes_", "sandron", "kkanoyaa", "desuqe_",
-    "dahujka_owo", "hober38_", "modlessflash", "wavewyyy", "mitor0_",
-    "zxbatonzx"
+    "dahujka_owo", "hober38_", "wavewyyy", "mitor0_",
+    "zxbatonzx", "kartavkun"
 ]
-
-# initial_channels = ["kartavkun"]
-
-get_channels = 
-    "kartav__", "ivango", "senyawei", "v1llix_", "glebauster",
-    "godroponika", "quizzzzz_", "vudek_", "pokemonyaaa", "25mosey",
-    "majewskiosu", "f0rz__", "steisha_owo", "sor0k4", "skyfai_",
-    "zoomqge", "lofkes_", "sandron", "kkanoyaa", "desuqe_",
-    "dahujka_owo", "hober38_", "modlessflash", "wavewyyy", "mitor0_",
-    "zxbatonzx"
-
-# get_channels = "kartavkun"
 
 dobri_link = "https://dobri.fun"
 
@@ -38,7 +26,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         super().__init__(token=API_TOKEN, prefix='!', initial_channels=initial_channels)
-        self.streaming = False
+        self.streaming_channels = set()
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
@@ -47,29 +35,34 @@ class Bot(commands.Bot):
     async def check_stream_status(self):
         while True:
             streams = await self.fetch_streams(user_logins=initial_channels)
-            if streams and not self.streaming:
-                self.streaming = True
-                await self.send_message('Стрим начался! Присоединяйтесь!')
-                print("Стрим запущен")
+            current_streaming_channels = {stream.user.name for stream in streams}  # Используем stream.user.name
+
+        # Проверяем, какие каналы начали или прекратили стрим
+            new_streaming_channels = current_streaming_channels - self.streaming_channels
+            stopped_streaming_channels = self.streaming_channels - current_streaming_channels
+
+            if new_streaming_channels:
+                self.streaming_channels.update(new_streaming_channels)
+                print("Стрим запущен для:", new_streaming_channels)
                 self.send_twitter_link.start()  # Запускаем рутину отправки ссылки
-            elif not streams and self.streaming:
-                self.streaming = False
-                await self.send_message('Стрим завершен. Спасибо за просмотр!')
-                print("Стрим отключен")
-                self.send_twitter_link.stop()  # Останавливаем рутину отправки ссылки
-            await asyncio.sleep(10)  # Проверять каждые 60 секунд
 
-    @routines.routine(seconds=15)  # Отправка сообщения каждые 30 секунд
+            if stopped_streaming_channels:
+                self.streaming_channels.difference_update(stopped_streaming_channels)
+                print("Стрим остановлен для:", stopped_streaming_channels)
+            if not self.streaming_channels:
+                self.send_twitter_link.stop()  # Останавливаем рутину, если нет стримов
+
+            await asyncio.sleep(60)  # Проверять каждые 60 секунд
+
+    @routines.routine(minutes=15)  # Отправка сообщения каждые 15 минут
     async def send_twitter_link(self):
-        await self.send_message(f'Все участники DOBRI сквада, ссылки на чаты в Telegram и Discord можешь найти здесь -> {dobri_link}')
-        print("Ссылка на сайт ДОБРИ отправлена")
-
-    async def send_message(self, message):
-        channel = self.get_channel(get_channels)
-        if channel:
-            await channel.send(message)
-        else:
-            print("Канал не найден!")
+        for channel_name in self.streaming_channels:
+            channel = self.get_channel(channel_name)
+            if channel:
+                await channel.send(f'Все участники DOBRI сквада, ссылки на чаты в Telegram и Discord можешь найти здесь -> {dobri_link}')
+                print(f"Ссылка на сайт ДОБРИ отправлена в {channel_name}")
+            else:
+                print(f"Канал {channel_name} не найден!")
 
     @commands.command(name='dobri')
     async def dobri(self, ctx: commands.Context):
